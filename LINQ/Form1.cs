35aff298958b28c25d9a1530c4e4690c74c736fc
Yosselin Flores <yoshy.floresg17@gmail.com>
2021-09-09 10:52:14 -1100

    Agregar archivos de proyecto.

---
+++ LINQ/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINQ
+{
+  
+    public partial class students : Form
+    {
+        public students()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetNames_Click(object sender, EventArgs e)
+        {
+            //llamado al metodo
+            selectName();
+        }
+        //metodo para obtener los nombres de la coleccion
+        private void selectName()
+        {
+            //consulta
+            List<string> query = (
+                from student in Student.GetStudents()
+                select student.firstName
+                ).ToList();
+            //recorrer lista y llenar lstNames
+            foreach (var name in query)
+            {
+                //llenar lstNames
+                lstNames.Items.Add(name);
+            }
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            //agregar columnas al datagriview
+            dgData.Columns.Add("idStudent", "CODIGO");
+            dgData.Columns.Add("firstName", "NOMBRE");
+            dgData.Columns.Add("lastName", "APELLIDO");
+            dgData.Columns.Add("age", "EDAD");
+            dgData.Columns.Add("nacionalidad", "NACIONALIDAD");
+            foreach(var student in selectAllData())
+            {
+                dgData.Rows.Add(student.idStudent, student.firstName, student.lastName, student.age, student.nacionalidad);
+            }
+        }
+        //metodo para consultar todos los datos de la coleccion
+        private List<Student> selectAllData()
+        {
+            List<Student> query = (
+                from student in Student.GetStudents()
+                select new Student()
+                {
+                    idStudent = student.idStudent,
+                    firstName = student.firstName,
+                    lastName = student.lastName,
+                    age = student.age,
+                    nacionalidad=student.nacionalidad
+                }
+                ).ToList();
+            return query;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+    }
+
---
+++ LINQ/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Student
+    {
+        public int idStudent { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+        public int age { get; set; }
+       public string nacionalidad { get; set; }
+        //metodo con la cloleccion de datos
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{idStudent= 1001, firstName = "Jose", lastName = "Robles", age = 25, nacionalidad="Colombiano" },
+                new Student{idStudent= 1002, firstName = "Ana", lastName = "Lopez", age = 20, nacionalidad="Costarisence" },
+                new Student{idStudent= 1003, firstName = "Rafael", lastName = "Gonzalez", age = 21, nacionalidad="Mexicano" },
+                new Student{idStudent= 1004, firstName = "Alfredo", lastName = "Valladares", age = 23, nacionalidad="Hondureño" },
+                new Student{idStudent= 1005, firstName = "Amanda", lastName = "Flores", age = 22, nacionalidad="Salvadoreña" },
+            };
+            return students; //retornamos la colección
+        }
+    }
+}
